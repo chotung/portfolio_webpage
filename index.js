d14d249ee6e9be5c7aebeb0584160092bcfca008
chotung <38818833+chotung@users.noreply.github.com>
2021-03-03 09:46:35 -0500

    inital page setup

--- index.js
+++ index.js
@@ -1,9 +1,20 @@
-const projectSection = document.querySelector("#project")
+// const mobileMenu = document.getElementById("mobile-menu");
+// const mobileMenuGroup = document.querySelector(".mobile-menu-group")
 
-const projectUpdater = () => {
-    
-}
+// document.addEventListener("DOMContentLoaded", () => {
+// 	const typed = new Typed('#intro', {
+// 		strings: ['Welcome', 'Charles Tung', 'Web Engineer' ],
+// 		typeSpeed: 100,
+// 		loop: true,
+// 		loopCount: Infinity,
+//   });
+// 	mobileMenu.addEventListener('click', toggleMobileMenu)
+// })
 
-document.addEventListener("DOMContentLoaded", () => {
-    console.log(projectSection);
-})
+// function toggleMobileMenu() {
+// 	if (mobileMenuGroup.style.display === "block") {
+// 		mobileMenuGroup.style.display = "none";
+// 	} else {
+// 		mobileMenuGroup.style.display = "block";
+// 	}
+// }
